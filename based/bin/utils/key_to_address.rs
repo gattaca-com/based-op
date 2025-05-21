6d29f0c03c3b02eaac6b5419dfa88315812bbceb
Louis Ponet <louisponet@gmail.com>
2025-05-21 23:38:45 +0100

    key to address generating random private key

--- based/bin/utils/key_to_address.rs
+++ based/bin/utils/key_to_address.rs
@@ -5,11 +5,17 @@ use revm_primitives::B256;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Usage: {} <private-key>", args[0]);
-        std::process::exit(1);
-    }
+    let private_key = if args.len() >= 2 {
+        B256::from_str(&args[1]).expect("wrong key format")
+    } else if args.len() == 1{
+        B256::random()
+    } else{
+        panic!("Don't know how to handle {} args", args.len())
+    };
+
     let address =
-        ECDSASigner::new(B256::from_str(&args[1]).expect("wrong key format")).expect("wrong key format").address;
-    println!("{}", address)
+            ECDSASigner::new(private_key).expect("wrong key format").address;
+
+    println!("{}", private_key);
+    println!("{}", address);
 }
