f2c661ebbb421ebd3edaaf2ed5cd321087204a9c
Suthiwat <suthiwatum@gmail.com>
2025-07-14 17:48:17 +0100

    kinda work now

--- based/bin/txproxy/src/main.rs
+++ based/bin/txproxy/src/main.rs
@@ -1,8 +1,14 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    str::FromStr,
+    time::Duration,
+};
 
 use bop_common::utils::init_tracing;
 use clap::Parser;
-use cli::TxProxy;
+use cli::TxProxyArgs;
+use jsonrpsee::http_client::{HttpClient, HttpClientBuilder};
+use reqwest::Url;
 use server::TxProxyServer;
 use tracing::info;
 
@@ -12,13 +18,105 @@ mod server;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    let args = TxProxy::parse();
+    let args = TxProxyArgs::parse();
     let _guard = init_tracing((&args).into());
 
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), args.portal_port);
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), args.txproxy_port);
     let server = TxProxyServer::new(args.clone()).await?;
 
-    info!(%addr, registry_url = %args.registry_url, fallback_url = %args.fallback_url, fallback_eth_url = %args.fallback_eth_url, "starting Based Portal");
+    server.add_forwarding_client(create_client(
+        Url::from_str("http://localhost:8545/").unwrap(),
+        Duration::from_millis(1000),
+    )?);
+    server.add_forwarding_client(create_client(
+        Url::from_str("http://localhost:18545/").unwrap(),
+        Duration::from_millis(1000),
+    )?);
+    server.add_forwarding_client(create_client(
+        Url::from_str("http://localhost:9995/").unwrap(),
+        Duration::from_millis(1000),
+    )?);
+    server.add_forwarding_client(create_client(
+        Url::from_str("http://localhost:9994/").unwrap(),
+        Duration::from_millis(1000),
+    )?);
 
+
+
+    info!(%addr, "starting TxProxy server");
     server.run(addr).await
+}
+
+fn create_client(url: Url, timeout: Duration) -> eyre::Result<HttpClient> {
+    let client = HttpClientBuilder::default()
+        .max_request_size(u32::MAX)
+        .max_response_size(u32::MAX)
+        .request_timeout(timeout.into())
+        .build(url)?;
+    Ok(client)
+}
+
+//
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use alloy::{
+        network::{EthereumWallet, TransactionBuilder, eip2718::Encodable2718},
+        primitives::U256,
+        providers::{Provider, ProviderBuilder},
+        rpc::types::TransactionRequest,
+        signers::local::PrivateKeySigner,
+    };
+    use alloy_primitives::Address;
+    use eyre::Result;
+
+    #[tokio::test]
+    async fn test_send_raw_transaction_to_flashbots() -> Result<()> {
+        let rpc_url = "http://localhost:8090".parse()?;
+        let rpc_full_url = "http://localhost:8545".parse()?;
+
+        let provider = ProviderBuilder::new().on_http(rpc_url);
+        let provider_full = ProviderBuilder::new().on_http(rpc_full_url);
+
+        let signer =
+            PrivateKeySigner::from_str("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80").unwrap();
+        let wallet = EthereumWallet::from(signer.clone());
+        let mut current_nonce = provider_full.get_transaction_count(signer.address()).await?;
+        
+        loop {
+            let bob = Address::from_str("0xF07208aB3090856D8444F2682a6ecd12832a2944").unwrap();
+            let tx = TransactionRequest::default()
+                .with_to(bob)
+                .with_nonce(current_nonce)
+                .with_chain_id(63)
+                .with_value(U256::from(100))
+                .with_gas_limit(21_000)
+                .with_max_priority_fee_per_gas(1_000_000_000)
+                .max_fee_per_gas(20_000_000_000);
+    
+            let tx_envelope = tx.build(&wallet).await?;
+            let tx_encoded = tx_envelope.encoded_2718();
+            
+            match provider.send_raw_transaction(&tx_encoded).await {
+                Ok(tx_hash) => {
+                    println!("Transaction sent successfully: {:?}", tx_hash);
+                }
+                Err(e) => {
+                    eprintln!("Error sending transaction: {:?}", e);
+                }
+            };
+
+            current_nonce += 1;
+
+            // if (current_nonce % 10) == 0 {
+            //     current_nonce = provider_full.get_transaction_count(signer.address()).await?;
+            // }
+
+            tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+        }
+
+
+        Ok(())
+    }
 }
--- based/bin/txproxy/src/middleware.rs
+++ based/bin/txproxy/src/middleware.rs
@@ -1,168 +1,97 @@
+use std::sync::Arc;
+
 use futures::{FutureExt, future::BoxFuture};
 use jsonrpsee::{
     MethodResponse,
-    core::{client::ClientT, traits::ToRpcParams},
+    core::{client::ClientT, params, traits::ToRpcParams},
+    http_client::HttpClient,
     server::middleware::rpc::RpcServiceT,
     types::{ErrorObject, Params, Request, ResponsePayload, error::INTERNAL_ERROR_CODE},
 };
+use parking_lot::RwLock;
 use serde_json::value::RawValue;
-use tracing::{debug, error};
+use tracing::{debug, error, info};
 
 use crate::server::{AuthRpcClient, RpcClient};
 
 #[derive(Clone)]
-pub struct ProxyService<S> {
-    supported_methods: &'static [&'static str],
-    inner: S,
-    fallback_eth_client: RpcClient,
-    fallback_client: AuthRpcClient,
-    op_client: RpcClient,
-    registry_client: RpcClient,
+pub struct MultiplexingService {
+    forwarding_to: Arc<RwLock<Vec<HttpClient>>>,
 }
 
-impl<S> ProxyService<S> {
-    pub fn new(
-        supported_methods: &'static [&'static str],
-        inner: S,
-        fallback_eth_client: RpcClient,
-        fallback_client: AuthRpcClient,
-        op_client: RpcClient,
-        registry_client: RpcClient,
-    ) -> Self {
-        Self { supported_methods, inner, fallback_eth_client, fallback_client, op_client, registry_client }
+impl MultiplexingService {
+    pub fn new(forwarding_to: Arc<RwLock<Vec<HttpClient>>>) -> Self {
+        Self { forwarding_to }
     }
 }
 
-impl<'a, S> RpcServiceT<'a> for ProxyService<S>
-where
-    S: Send + Clone + Sync + RpcServiceT<'a> + 'a,
-{
+impl<'a> RpcServiceT<'a> for MultiplexingService {
     type Future = BoxFuture<'a, MethodResponse>;
 
     #[tracing::instrument(skip_all, name = "middleware")]
     fn call(&self, req: Request<'a>) -> Self::Future {
         //TODO: is this really the best way to do this?
-        let inner = self.inner.clone();
-        let fallback_client = self.fallback_client.clone();
-        let fallback_eth_client = self.fallback_eth_client.clone();
-        let registry_client = self.registry_client.clone();
-        let op_client = self.op_client.clone();
+        let forwarding_to_arc = Arc::clone(&self.forwarding_to);
 
-        let supported_methods = self.supported_methods;
+        let method = req.method_name().to_string();
+        let params_raw = req.params.unwrap().get().to_string();
 
         async move {
-            if &req.method_name()[0..=5] == "portal" || supported_methods.contains(&req.method_name()) {
-                debug!(method = %req.method_name(), "handling request");
-                inner.call(req).await
-            } else {
-                let params = WrapParams(req.params());
-                let r: Result<serde_json::Value, jsonrpsee::core::ClientError> = match req.method_name().split_once('_')
-                {
-                    Some(("engine", _)) => {
-                        debug!(method = %req.method_name(), "forwarding request to fallback");
-                        fallback_client.clone().request(req.method_name(), params).await
-                    }
-                    Some(("eth", _)) => {
-                        debug!(method = %req.method_name(), "forwarding request to eth fallback");
-                        fallback_eth_client.clone().request(req.method_name(), params).await
-                    }
-                    Some(("miner", _)) => {
-                        debug!(method = %req.method_name(), "forwarding request to eth fallback");
-                        fallback_eth_client.clone().request(req.method_name(), params).await
-                    }
-                    Some(("net", _)) => {
-                        debug!(method = %req.method_name(), "forwarding request to eth fallback");
-                        fallback_eth_client.clone().request(req.method_name(), params).await
-                    }
-                    Some(("web3", _)) => {
-                        debug!(method = %req.method_name(), "forwarding request to eth fallback");
-                        fallback_eth_client.clone().request(req.method_name(), params).await
-                    }
-                    Some(("admin", _)) => {
-                        debug!(method = %req.method_name(), "forwarding request to eth fallback");
-                        fallback_eth_client.clone().request(req.method_name(), params).await
-                    }
-                    Some(("txpool", _)) => {
-                        debug!(method = %req.method_name(), "forwarding request to eth fallback");
-                        fallback_eth_client.clone().request(req.method_name(), params).await
-                    }
-                    Some(("optimism", _)) | Some(("opp2p", _)) => {
-                        debug!(method = %req.method_name(), "forwarding request to op-node fallback");
-                        op_client.request(req.method_name(), params).await
-                    }
+            info!(method = %method, params = %params_raw, "Received request for method");
 
-                    Some(("node", rest)) => {
-                        debug!(method = %req.method_name(), "forwarding request to op-node");
-                        op_client.request(rest, params).await
-                    }
+            if method != "eth_sendRawTransaction" {
+                error!(method = %method, "Unsupported method for multiplexing: {}", method);
+                return MethodResponse::error(
+                    req.id,
+                    ErrorObject::owned(
+                        INTERNAL_ERROR_CODE,
+                        "Method not supported".to_string(),
+                        Some("Only eth_sendRawTransaction is supported".to_string()),
+                    ),
+                );
+            }
 
-                    Some(("geth", rest)) => {
-                        debug!(method = %req.method_name(), "forwarding request to op-geth");
-                        fallback_client.clone().request(rest, params).await
-                    }
-
-                    Some(("registry", _)) => {
-                        debug!(method = %req.method_name(), "forwarding request to registry");
-                        registry_client.request(req.method_name(), params).await
-                    }
-                    Some((_, _)) => {
-                        error!(method = %req.method_name(), "i don't know what to do with this");
-                        Err(jsonrpsee::core::ClientError::Custom("method nonexistent".to_string()))
-                    }
-                    None => {
-                        error!(id = %req.id ,"received empty request");
-                        Err(jsonrpsee::core::ClientError::Custom("empty request".to_string()))
-                    }
-                };
-
-                match r {
-                    Ok(r) => {
-                        let payload = ResponsePayload::success(r);
-                        MethodResponse::response(req.id, payload.into(), 4_000_000_000usize)
-                    }
-                    Err(_)
-                        if req.method_name() == "eth_getBlockByNumber" &&
-                            req.params.as_ref().is_some_and(|p| {
-                                p.to_string().contains("finalized") ||
-                                    p.to_string().contains("latest") ||
-                                    p.to_string().contains("safe")
-                            }) =>
-                    {
+            let clients_to_forward = forwarding_to_arc.read().clone();
+            let request_tasks = clients_to_forward
+                .into_iter()
+                .map(|client| {
+                    let method = method.clone();
+                    let params = WrapParams::from_raw(params_raw.clone());
+                    async move {
                         let r: Result<serde_json::Value, jsonrpsee::core::ClientError> =
-                            fallback_eth_client.clone().request("eth_getBlockByNumber", ("latest", false)).await;
+                            client.request(method.as_str(), params).await;
                         match r {
-                            Ok(r) => {
-                                tracing::warn!("Serving latest instead of finalized or safe for eth_getBlockByNumber. This should only happen at genesis!");
-                                let payload = ResponsePayload::success(r);
-                                MethodResponse::response(req.id, payload.into(), 4_000_000_000usize)
+                            Ok(value) => {
+                                debug!(response = ?value, "Request processed successfully (individual)");
+                                return Ok(value);
                             }
-                            Err(jsonrpsee::core::ClientError::Call(err)) => {
-                                MethodResponse::error(
-                                    req.id,
-                                    err,
-                                )
-                            }
-                            Err(err) => {
-                                error!(?err, "error forwarding {} request", req.method_name());
-                                MethodResponse::error(
-                                    req.id,
-                                    ErrorObject::borrowed(INTERNAL_ERROR_CODE, &format!("{err}"), None))
+                            Err(e) => {
+                                debug!(error = %e, "Error while processing request (individual)");
+                                return Err(e);
                             }
                         }
                     }
-                    Err(jsonrpsee::core::ClientError::Call(err)) => {
-                        MethodResponse::error(
-                            req.id,
-                            err,
-                        )
-                    }
-                    Err(err) => {
-                        error!(?err, "error forwarding {} request", req.method_name());
+                    .boxed()
+                })
+                .collect::<Vec<_>>();
 
-                                MethodResponse::error(
-                                    req.id,
-                                    ErrorObject::borrowed(INTERNAL_ERROR_CODE, &format!("{err}"), None))
-                    }
+            let responses = futures::future::select_ok(request_tasks).await;
+
+            match responses {
+                Ok((value, other_tasks)) => {
+                    let payload = ResponsePayload::success(&value);
+                    let _ = tokio::task::spawn(async move {
+                        futures::future::join_all(other_tasks).await;
+                    });
+                    info!(response = ?payload, "Request processed successfully");
+                    return MethodResponse::response(req.id, payload.into(), 4_000_000_000usize);
+                }
+                Err(e) => {
+                    error!(error = %e, "All clients failed to process request");
+                    return MethodResponse::error(
+                        req.id,
+                        ErrorObject::owned(INTERNAL_ERROR_CODE, "Internal error".to_string(), Some(e.to_string())),
+                    );
                 }
             }
         }
@@ -171,139 +100,17 @@ where
 }
 
 // TODO: remove this
-struct WrapParams<'a>(Params<'a>);
-impl ToRpcParams for WrapParams<'_> {
-    fn to_rpc_params(self) -> Result<Option<Box<RawValue>>, serde_json::Error> {
-        // FIXME: we should not clone here
-        self.0.as_str().map(String::from).map(RawValue::from_string).transpose()
+#[derive(Debug, Clone)]
+struct WrapParams(String);
+
+impl WrapParams {
+    fn from_raw(params_raw: String) -> Self {
+        Self(params_raw)
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use std::{
-        net::SocketAddr,
-        sync::{Arc, atomic::AtomicBool},
-    };
-
-    use jsonrpsee::{
-        RpcModule,
-        http_client::{HttpClient, HttpClientBuilder},
-        server::{RpcServiceBuilder, ServerBuilder},
-    };
-    use reth_rpc_layer::{AuthClientLayer, JwtSecret};
-
-    use super::*;
-
-    #[ignore = "Requires RPC calls"]
-    #[tokio::test]
-    async fn test_proxy() {
-        let received_fallback = Arc::new(AtomicBool::new(false));
-        let received_eth_fallback = Arc::new(AtomicBool::new(false));
-        let received_mux = Arc::new(AtomicBool::new(false));
-
-        let fallback_server = ServerBuilder::default()
-            .max_request_body_size(u32::MAX)
-            .max_response_body_size(u32::MAX)
-            .build("127.0.0.1:9090".parse::<SocketAddr>().unwrap())
-            .await
-            .unwrap();
-        let mut module = RpcModule::new(());
-        let rcv = received_fallback.clone();
-        module
-            .register_method("engine_fallback", move |_, _, _| {
-                rcv.store(true, std::sync::atomic::Ordering::Relaxed);
-            })
-            .unwrap();
-
-        let _fallback_handle = fallback_server.start(module);
-
-        let eth_fallback_server = ServerBuilder::default()
-            .max_request_body_size(u32::MAX)
-            .max_response_body_size(u32::MAX)
-            .build("127.0.0.1:9091".parse::<SocketAddr>().unwrap())
-            .await
-            .unwrap();
-        let mut eth_module = RpcModule::new(());
-        let rcv = received_eth_fallback.clone();
-        eth_module
-            .register_method("eth_fallback", move |_, _, _| {
-                rcv.store(true, std::sync::atomic::Ordering::Relaxed);
-            })
-            .unwrap();
-
-        let _eth_fallback_handle = eth_fallback_server.start(eth_module);
-
-        let secret_layer = AuthClientLayer::new(JwtSecret::random());
-        let middleware = tower::ServiceBuilder::default().layer(secret_layer);
-        let fallback_client = HttpClientBuilder::default()
-            .max_request_size(u32::MAX)
-            .max_response_size(u32::MAX)
-            .set_http_middleware(middleware)
-            .build("http://127.0.0.1:9090")
-            .unwrap();
-
-        let fallback_eth_client = HttpClientBuilder::default()
-            .max_request_size(u32::MAX)
-            .max_response_size(u32::MAX)
-            .build("http://127.0.0.1:9091")
-            .unwrap();
-
-        let op_client = HttpClientBuilder::default()
-            .max_request_size(u32::MAX)
-            .max_response_size(u32::MAX)
-            .build("http://127.0.0.1:9545")
-            .unwrap();
-        let registry_client = HttpClientBuilder::default()
-            .max_request_size(u32::MAX)
-            .max_response_size(u32::MAX)
-            .build("http://127.0.0.1:8081")
-            .unwrap();
-
-        let rpc_middleware = RpcServiceBuilder::new().layer_fn(move |s| {
-            ProxyService::new(
-                &["hello_mux"],
-                s,
-                fallback_eth_client.clone(),
-                fallback_client.clone(),
-                op_client.clone(),
-                registry_client.clone(),
-            )
-        });
-
-        let mux_server = ServerBuilder::default()
-            .max_request_body_size(u32::MAX)
-            .max_response_body_size(u32::MAX)
-            .set_rpc_middleware(rpc_middleware)
-            .build("127.0.0.1:9092".parse::<SocketAddr>().unwrap())
-            .await
-            .unwrap();
-        let mut mux_module = RpcModule::new(());
-
-        let rcv = received_mux.clone();
-        mux_module
-            .register_method("hello_mux", move |_, _, _| {
-                rcv.store(true, std::sync::atomic::Ordering::Relaxed);
-            })
-            .unwrap();
-
-        let _mux_server_handle = mux_server.start(mux_module);
-
-        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-
-        let client = HttpClient::builder()
-            .max_request_size(u32::MAX)
-            .max_response_size(u32::MAX)
-            .build("http://127.0.0.1:9092")
-            .unwrap();
-
-        let _: serde_json::Value = client.request("hello_mux", vec![""]).await.unwrap();
-        assert!(received_mux.load(std::sync::atomic::Ordering::Relaxed));
-
-        let _: serde_json::Value = client.request("engine_fallback", vec![""]).await.unwrap();
-        assert!(received_fallback.load(std::sync::atomic::Ordering::Relaxed));
-
-        let _: serde_json::Value = client.request("eth_fallback", vec![""]).await.unwrap();
-        assert!(received_eth_fallback.load(std::sync::atomic::Ordering::Relaxed));
+impl ToRpcParams for WrapParams {
+    fn to_rpc_params(self) -> Result<Option<Box<RawValue>>, serde_json::Error> {
+        Ok(Some(RawValue::from_string(self.0).unwrap()))
     }
 }
