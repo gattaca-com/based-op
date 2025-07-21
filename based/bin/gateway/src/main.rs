d11b67c995d37910ee4dce1be4cbf9258b01dc34
Suthiwat Umpornpaiboon <91750004+BrycePy@users.noreply.github.com>
2025-07-21 22:43:02 +0100

    allow revert config (#193)

--- based/bin/gateway/src/main.rs
+++ based/bin/gateway/src/main.rs
@@ -117,7 +117,7 @@ fn run(args: GatewayArgs) -> eyre::Result<()> {
                 let connections = spine.to_connections(format!("Simulator-{id}"));
                 let db_frag = (&shared_state).into();
                 move || {
-                    let simulator = Simulator::new(db_frag, evm_config, id);
+                    let simulator = Simulator::new(db_frag, evm_config, id, args.allow_reverts);
                     simulator.run(connections, ActorConfig::default());
                 }
             });
