461262022c61626e7110faf79ef863c0d6b9893d
nicolas <48695862+merklefruit@users.noreply.github.com>
2025-08-19 14:01:11 +0200

    feat(metrics): add telemetry queue metrics

--- based/crates/metrics/src/consumer.rs
+++ based/crates/metrics/src/consumer.rs
@@ -2,7 +2,7 @@ use std::time::Duration;
 
 use bop_common::{
     communication::Consumer,
-    telemetry::{Telemetry, TelemetryUpdate, Tx, system::SystemNotification, telemetry_queue},
+    telemetry::{Frag, Telemetry, TelemetryUpdate, Tx, system::SystemNotification, telemetry_queue},
 };
 use tracing::trace;
 
@@ -16,14 +16,14 @@ pub struct MetricsConsumer {
 impl MetricsConsumer {
     /// Runs the metrics consumer, consuming telemetry updates from shared queues,
     /// and converting them into metrics.
-    pub async fn run(&mut self) {
+    pub async fn run(mut self) {
         loop {
             while let Some(update) = self.telemetry.try_consume() {
                 trace!(?update, "Received telemetry update");
                 self.process_update(update);
             }
 
-            tokio::time::sleep(Duration::from_millis(50)).await
+            tokio::time::sleep(Duration::from_millis(20)).await
         }
     }
 
@@ -32,24 +32,34 @@ impl MetricsConsumer {
         match update.update {
             Telemetry::Tx(tx) => match tx {
                 Tx::AddedToPool => Metrics::increase_gateway_tx_added_to_pool_total(),
+                Tx::RemovedFromPool => Metrics::increase_gateway_tx_removed_from_pool_total(),
                 Tx::Included(_) => Metrics::increase_gateway_tx_included_total(),
-                _ => {
-                    // TODO
-                }
+                Tx::Ingested(_) => Metrics::increase_gateway_tx_ingested_total(),
             },
             Telemetry::System(system) => match system {
                 SystemNotification::StateChanged(state) => Metrics::set_sequencer_state(state),
-                SystemNotification::BlockSync(block_number, _gas_used) => {
-                    Metrics::set_block_sync_block_number(block_number)
-                }
-                SystemNotification::Sorting(block_number) => Metrics::set_sorting_block_number(block_number),
-                _ => {
-                    // TODO
-                }
+                SystemNotification::BlockSync(block_num, _) => Metrics::set_block_sync_block_number(block_num),
+                SystemNotification::Sorting(block_num) => Metrics::set_sorting_block_number(block_num),
+                SystemNotification::BuildStop(block_num) => Metrics::set_build_stop_block_number(block_num),
+                SystemNotification::NewPayload(block_num) => Metrics::set_new_payload_block_number(block_num),
+                SystemNotification::GetPayload(block_num) => Metrics::set_get_payload_block_number(block_num),
+                SystemNotification::ForkChoiceUpdate(_block_hash) => { /* event skipped*/ }
             },
-            _ => {
-                // TODO
-            }
+            Telemetry::Frag(frag) => match frag {
+                Frag::SorterStart { block, available_value, .. } => {
+                    Metrics::increase_frag_sorter_start_total();
+                    Metrics::set_frag_current_block_number(block);
+                    Metrics::record_frag_available_value(available_value.into());
+                }
+                Frag::SorterFinish { payment, best_order_value, n_txs, gas_used, .. } => {
+                    Metrics::increase_frag_sorter_finish_total();
+                    Metrics::record_frag_payment(payment.into());
+                    Metrics::record_frag_gas_used(gas_used);
+                    Metrics::record_frag_transaction_count(n_txs);
+                    Metrics::record_frag_best_order_value(best_order_value.into());
+                }
+                Frag::Commit => Metrics::increase_sequencer_commit_frag_total(),
+            },
         }
     }
 }
--- based/crates/metrics/src/metrics.rs
+++ based/crates/metrics/src/metrics.rs
@@ -1,5 +1,5 @@
 use bop_common::telemetry::system::SequencerState;
-use metrics::{counter, gauge};
+use metrics::{counter, gauge, histogram};
 
 /// Contains all metrics for the based monitoring stack.
 #[derive(Default)]
@@ -10,8 +10,16 @@ impl Metrics {
         counter!("bop_gateway_tx_added_to_pool_total").increment(1);
     }
 
+    pub fn increase_gateway_tx_removed_from_pool_total() {
+        counter!("bop_gateway_tx_removed_from_pool_total").increment(1);
+    }
+
     pub fn increase_gateway_tx_included_total() {
         counter!("bop_gateway_tx_included_total").increment(1);
+    }
+
+    pub fn increase_gateway_tx_ingested_total() {
+        counter!("bop_gateway_tx_ingested_total").increment(1);
     }
 
     pub fn set_sequencer_state(state: SequencerState) {
@@ -24,5 +32,53 @@ impl Metrics {
 
     pub fn set_sorting_block_number(block_number: u64) {
         gauge!("bop_sorting_block_number").set(block_number as f64);
+    }
+
+    pub fn set_build_stop_block_number(block_number: u64) {
+        gauge!("bop_build_stop_block_number").set(block_number as f64);
+    }
+
+    pub fn set_new_payload_block_number(block_number: u64) {
+        gauge!("bop_new_payload_block_number").set(block_number as f64);
+    }
+
+    pub fn set_get_payload_block_number(block_number: u64) {
+        gauge!("bop_get_payload_block_number").set(block_number as f64);
+    }
+
+    pub fn increase_sequencer_commit_frag_total() {
+        counter!("bop_sequencer_commit_frag_total").increment(1);
+    }
+
+    pub fn record_frag_available_value(available_value: u64) {
+        histogram!("bop_frag_available_value").record(available_value as f64);
+    }
+
+    pub fn increase_frag_sorter_start_total() {
+        counter!("bop_frag_sorter_start_total").increment(1);
+    }
+
+    pub fn set_frag_current_block_number(block_number: u64) {
+        gauge!("bop_frag_current_block_number").set(block_number as f64);
+    }
+
+    pub fn increase_frag_sorter_finish_total() {
+        counter!("bop_frag_sorter_finish_total").increment(1);
+    }
+
+    pub fn record_frag_payment(payment: u64) {
+        histogram!("bop_frag_payment").record(payment as f64);
+    }
+
+    pub fn record_frag_gas_used(gas_used: u64) {
+        histogram!("bop_frag_gas_used").record(gas_used as f64);
+    }
+
+    pub fn record_frag_transaction_count(transaction_count: usize) {
+        histogram!("bop_frag_transaction_count").record(transaction_count as f64);
+    }
+
+    pub fn record_frag_best_order_value(best_order_value: u64) {
+        histogram!("bop_frag_best_order_value").record(best_order_value as f64);
     }
 }
