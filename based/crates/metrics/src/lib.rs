e9037a064dc1961171fc9b120e8365a17d893c5b
nicolas <48695862+merklefruit@users.noreply.github.com>
2025-08-20 09:59:22 +0200

    feat(metrics): add bop-metrics crate skeleton (#203)

---
+++ based/crates/metrics/src/lib.rs
@@ -0,0 +1,13 @@
+use metrics_exporter_prometheus::PrometheusBuilder;
+
+pub mod consumer;
+pub mod metrics;
+
+/// Installs the prometheus exporter on the given port.
+pub fn install_prometheus_exporter(port: u16) {
+    PrometheusBuilder::new()
+        .with_http_listener(([0, 0, 0, 0], port))
+        .add_global_label("instance", "based-optimism")
+        .install()
+        .expect("Failed to install prometheus exporter");
+}
