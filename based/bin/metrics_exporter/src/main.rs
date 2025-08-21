2a925889984e8dfbad7e1db1d26b90da54a89e62
nicolas <48695862+merklefruit@users.noreply.github.com>
2025-08-21 16:08:15 +0200

    feat(metrics): setup basic monitoring infra for local dev (#205)

--- based/bin/metrics_exporter/src/main.rs
+++ based/bin/metrics_exporter/src/main.rs
@@ -1,5 +1,7 @@
 use bop_metrics::{consumer::MetricsConsumer, install_prometheus_exporter};
 use clap::Parser;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 /// Exposes metrics for Prometheus to scrape.
 #[derive(Debug, Parser)]
@@ -11,9 +13,17 @@ struct Args {
 
 #[tokio::main]
 async fn main() {
+    // Parse CLI arguments
     let args = Args::parse();
 
-    install_prometheus_exporter(args.port);
+    // Init tracing
+    let env = EnvFilter::builder().with_default_directive(LevelFilter::INFO.into()).from_env_lossy();
+    tracing_subscriber::registry().with(env).with(tracing_subscriber::fmt::layer()).init();
 
+    // Init Prometheus server
+    install_prometheus_exporter(args.port);
+    info!("Prometheus server started on port {}", args.port);
+
+    // Start the metrics consumer loop
     MetricsConsumer::default().run().await;
 }
