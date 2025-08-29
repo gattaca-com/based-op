a42f51711c4ddde3e417428c50fd80173744118d
Suthiwat <suthiwatum@gmail.com>
2025-08-29 16:04:55 +0100

    lifetime bug + clippy + fmt

--- based/bin/gateway/src/main.rs
+++ based/bin/gateway/src/main.rs
@@ -65,6 +65,9 @@ fn run(args: GatewayArgs) -> eyre::Result<()> {
     let evm_config = sequencer_config.evm_config.clone();
     let (frag_broadcast_tx, _) = tokio::sync::broadcast::channel(10_000);
 
+    let sequencer_vsync_window = Duration::from_micros(args.vsync_window_us as u64);
+    let simulator_vsync_window = Duration::from_micros(args.vsync_window_us as u64);
+
     std::thread::scope(|s| {
         let rt: Arc<Runtime> = tokio::runtime::Builder::new_current_thread()
             .worker_threads(10)
@@ -80,8 +83,10 @@ fn run(args: GatewayArgs) -> eyre::Result<()> {
         });
 
         s.spawn(|| {
-            Sequencer::new(db_bop, shared_state.clone(), sequencer_config)
-                .run(spine.to_connections("Sequencer"), ActorConfig::default().with_min_loop_duration(Duration::from_micros(args.vsync_window_us as u64)));
+            Sequencer::new(db_bop, shared_state.clone(), sequencer_config).run(
+                spine.to_connections("Sequencer"),
+                ActorConfig::default().with_min_loop_duration(sequencer_vsync_window),
+            );
         });
 
         if let Some(mode) = args.mock {
@@ -121,8 +126,7 @@ fn run(args: GatewayArgs) -> eyre::Result<()> {
                 let evm_config = evm_config.clone();
                 let connections = spine.to_connections(format!("Simulator-{id}"));
                 let db_frag = (&shared_state).into();
-                let min_loop_duration = Duration::from_micros(args.vsync_window_us as u64);
-                let actor_config = ActorConfig::default().with_min_loop_duration(min_loop_duration);
+                let actor_config = ActorConfig::default().with_min_loop_duration(simulator_vsync_window);
                 move || {
                     let simulator = Simulator::new(db_frag, evm_config, id, args.allow_reverts);
                     simulator.run(connections, actor_config);
