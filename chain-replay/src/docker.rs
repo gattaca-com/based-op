29f18dec4c5cc7f411288232cbdcaf3409f03c2b
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2025-08-29 10:50:07 +0200

    feat(chain-replay): generate config files for every supported chain

--- chain-replay/src/docker.rs
+++ chain-replay/src/docker.rs
@@ -1,51 +1,26 @@
-use std::{
-    io,
-    process::{self, Command},
-};
+use std::{io, process::Command};
 
 use alloy::primitives::B256;
 
-use crate::chain::ChainName;
+use crate::{chain::ChainName, utils::output_to_result};
 
-/// Creates a basic `docker compose` command to spin a up a service for the given chain.
-/// Returns a command just without the service name and any additional environment variables.
-pub fn basic_compose_command(chain_name: ChainName) -> Command {
+/// Starts the based-op-geth service for the given chain, setting the sync target hash
+pub fn start_based_op_service(chain_name: ChainName, sync_target_hash: B256) -> io::Result<()> {
     let compose_file_path = chain_name.compose_file_path();
     let env_file_path = chain_name.env_file_path();
 
-    let mut command = Command::new("docker");
-    command
-        .arg("compose")
-        .arg("--file")
-        .arg(compose_file_path)
-        .arg("--env-file")
-        .arg(env_file_path)
-        .arg("up")
-        .arg("-d");
-    command
-}
+    let command_str = format!(
+        "docker compose --file {} --env-file {} up -d based-op-geth",
+        compose_file_path.to_string_lossy(),
+        env_file_path.to_string_lossy()
+    );
+    let mut args = command_str.trim_matches('"').split(' ');
 
-pub fn start_based_op_service(chain_name: ChainName, sync_target_hash: B256) -> io::Result<()> {
-    let command = basic_compose_command(chain_name)
-        .arg("based-op-geth")
+    let mut command = Command::new(args.next().expect("docker"));
+    let output = command
+        .args(args)
         .env("OP_GETH_SYNC_TARGET", format!("{sync_target_hash:?}"))
         .output()?;
 
-    output_to_result(command, "based-op-geth")?;
-
-    Ok(())
-}
-
-fn output_to_result(output: process::Output, service: &str) -> io::Result<()> {
-    if output.status.success() {
-        let text = String::from_utf8_lossy(&output.stdout);
-        println!("Service '{service}' started successfully: {text}");
-    } else {
-        let text = String::from_utf8_lossy(&output.stderr);
-        return Err(io::Error::other(format!(
-            "Failed to start service '{service}': {}",
-            text
-        )));
-    }
-    Ok(())
+    output_to_result(&command_str, output)
 }
--- chain-replay/src/utils.rs
+++ chain-replay/src/utils.rs
@@ -1,4 +1,7 @@
-use std::{fs, io};
+use std::{
+    fs, io,
+    process::{self, Command},
+};
 
 use crate::chain::ChainName;
 
@@ -37,7 +40,96 @@ pub fn ensure_chain_folder(chain_name: ChainName) -> io::Result<()> {
         fs::copy("compose.yml", compose_path)?;
     }
 
-    // TODO: add genesis.json and rollup.json
+    let genesis_path = chain_name.genesis_file_path();
+    if !fs::exists(genesis_path.clone())? {
+        println!(
+            "Creating genesis.json file for chain '{chain_name}' at: {:?}",
+            genesis_path.clone()
+        );
+        generate_genesis_file(chain_name)?;
+    }
+
+    let rollup_path = chain_name.rollup_file_path();
+    if !fs::exists(rollup_path.clone())? {
+        println!(
+            "Creating rollup.json file for chain '{chain_name}' at: {:?}",
+            rollup_path.clone()
+        );
+        generate_rollup_file(chain_name)?;
+    }
 
     Ok(())
+}
+
+/// For the given chain, generate a valid genesis.json file using the based-op-geth image.
+/// For chains part of OP-superchain, it consists of running the `dumpgenesis` command.
+/// For based-op-sepolia, we'll copy the confguration inside the `sepolia-genesis` folder.
+pub fn generate_genesis_file(chain_name: ChainName) -> io::Result<()> {
+    match chain_name {
+        ChainName::BasedOpSepolia => fs::copy(
+            "../sepolia_genesis/genesis.json",
+            chain_name.genesis_file_path(),
+        )
+        .map(|_| ()),
+        chain => {
+            let command_str = format!(
+                "docker run ghcr.io/gattaca-com/based-op-geth/based-op-geth:latest --op-network {chain} dumpgenesis"
+            );
+            let mut args = command_str.split(' ');
+            let mut command = Command::new(args.next().expect("docker"));
+            let output = command.args(args).output()?;
+            let stdout = extract_stdout(&command_str, output)?;
+            fs::write(chain_name.genesis_file_path(), stdout)
+        }
+    }
+}
+
+/// For the given chain, generate a valid rollup.json file using the based-op-node image. For
+/// chains part of OP-superchain, it consists of running the `networks dump-rollup-config` command.
+/// For based-op-sepolia, we'll copy the confguration inside the `sepolia-genesis` folder.
+pub fn generate_rollup_file(chain_name: ChainName) -> io::Result<()> {
+    match chain_name {
+        ChainName::BasedOpSepolia => fs::copy(
+            "../sepolia_genesis/rollup.json",
+            chain_name.genesis_file_path(),
+        )
+        .map(|_| ()),
+        chain => {
+            let command_str = format!(
+                "docker run ghcr.io/gattaca-com/based-optimism/based-op-node:latest op-node networks dump-rollup-config --network {chain}"
+            );
+            let mut args = command_str.split(' ');
+            let mut command = Command::new(args.next().expect("docker"));
+            let output = command.args(args).output()?;
+            let stdout = extract_stdout(&command_str, output)?;
+            fs::write(chain_name.rollup_file_path(), stdout)
+        }
+    }
+}
+
+/// Converts a process::Output into an io::Result, printing the stdout on success
+pub fn output_to_result(command_str: &str, output: process::Output) -> io::Result<()> {
+    if output.status.success() {
+        let text = String::from_utf8_lossy(&output.stdout);
+        println!("Command {command_str} executed successfully: {text}");
+        Ok(())
+    } else {
+        let text = String::from_utf8_lossy(&output.stderr);
+        Err(io::Error::other(format!(
+            "Command {command_str} failed: {text}"
+        )))
+    }
+}
+
+/// Extracts stdout from a successfull output of a command, otherwise it returns an `io::Error`
+/// capturing stderr.
+pub fn extract_stdout(command_str: &str, output: process::Output) -> io::Result<Vec<u8>> {
+    if output.status.success() {
+        return Ok(output.stdout);
+    }
+
+    let text = String::from_utf8_lossy(&output.stderr);
+    Err(io::Error::other(format!(
+        "Command {command_str} failed: {text}"
+    )))
 }
