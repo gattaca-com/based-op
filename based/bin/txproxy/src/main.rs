e724a43cc673e9fca6ec6f3fc47df39de459d333
eltitanb <lorenzo@gattaca.com>
2025-09-10 13:29:02 +0100

    in process

--- based/bin/txproxy/src/main.rs
+++ based/bin/txproxy/src/main.rs
@@ -1,6 +1,7 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-use bop_common::utils::init_tracing;
+use bop_common::{metrics::install_prometheus_exporter, utils::init_tracing};
+use bop_metrics::MetricsConsumer;
 use clap::Parser;
 use cli::TxProxyArgs;
 use server::TxProxyServer;
@@ -16,6 +17,15 @@ async fn main() -> eyre::Result<()> {
 
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), args.txproxy_port);
     let server = TxProxyServer::new(args.clone()).await?;
+
+    if args.enable_metrics {
+        let consumer = MetricsConsumer::default();
+        tokio::spawn(async move {
+            install_prometheus_exporter(args.metrics_port);
+            info!("Prometheus server started on port {}", args.metrics_port);
+            consumer.run().await
+        });
+    }
 
     info!(%addr, "starting TxProxy server");
     server.run(addr).await
