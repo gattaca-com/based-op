fab0a40110582000869a59acfe9e7594a6442e87
Suthiwat <suthiwatum@gmail.com>
2025-09-11 15:57:52 +0100

    maybe apply before sealing

--- based/crates/sequencer/src/lib.rs
+++ based/crates/sequencer/src/lib.rs
@@ -551,7 +551,8 @@ impl<Db: Clone + DatabaseRef + DatabaseRead> SequencerState<Db> {
         use SequencerState::*;
         let base_fee = data.as_ref().basefee;
         match self {
-            Sorting(mut seq, sorting_data) if sorting_data.should_seal_frag() => {
+            Sorting(mut seq, mut sorting_data) if sorting_data.should_seal_frag() && sorting_data.should_send_next_sims() => {
+                sorting_data.maybe_apply(base_fee);
                 data.timers.waiting_for_sims.stop();
                 data.timers.seal_frag.start();
                 // Reset the tx pool.
