8cb83d49beb5b9d8bb01e4a6f2fe3b5962d80090
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2025-09-15 10:45:44 +0200

    fix(chain-replay): drop sleep between sending txs at all
    Otherwise with very large blocks of >500 txs we don't send all txs in
    time

--- chain-replay/src/driver.rs
+++ chain-replay/src/driver.rs
@@ -362,7 +362,6 @@ async fn run_verification_body(
                 let _ =
                     clients.gateway.send_raw_transaction(&t).await.expect("to send tx to gateway");
                 i += 1;
-                tokio::time::sleep(Duration::from_millis(1)).await;
             }
             tokio::time::sleep(Duration::from_secs(2)).await; // Let the timeout hit.
         })
