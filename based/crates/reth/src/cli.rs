32c66f03e01202a92f902dda40991b339f576cb0
Jonas Bostoen <jonasbostoen@fastmail.com>
2025-12-18 14:34:32 +0100

    feat(reth): scaffold engine API

--- based/crates/reth/src/cli.rs
+++ based/crates/reth/src/cli.rs
@@ -6,7 +6,8 @@ use reth_optimism_cli::{Cli, chainspec::OpChainSpecParser};
 use reth_optimism_node::{OpNode, args::RollupArgs};
 
 use crate::{
-    driver::{Driver, DriverInner},
+    api::engine::{BasedEngineApi, BasedEngineApiServer as _},
+    driver::Driver,
     exec::NoopExecutor,
 };
 
@@ -27,16 +28,19 @@ pub fn run() -> eyre::Result<()> {
 
         let op_node = OpNode::new(args.rollup.clone());
 
-        let _node_handle = builder
+        let node_handle = builder
             .with_types::<OpNode>()
             .with_components(op_node.components())
             .with_add_ons(op_node.add_ons())
             // Install the execution extension to handle canonical chain updates
             .install_exex("based-op", {
+                // Get a clone of the driver handle.
+                let driver = driver.clone();
                 move |mut ctx| async move {
                     Ok(async move {
                         while let Some(note) = ctx.notifications.try_next().await? {
                             if let Some(committed) = note.committed_chain() {
+                                // Handle committed blocks by notifying the driver
                                 for block in committed.blocks_iter() {
                                     driver.forkchoice_updated(block.clone().into_block()).await?;
                                 }
@@ -49,6 +53,10 @@ pub fn run() -> eyre::Result<()> {
                 }
             })
             .extend_rpc_modules(move |ctx| {
+                // Add based engine API modules to the existing auth module.
+                ctx.auth_module
+                    .merge_auth_methods(BasedEngineApi::new(driver).into_rpc())
+                    .expect("failed to merge modules");
                 // TODO:
                 // - Replace / extend the engine API
                 // - Replace eth API
@@ -57,7 +65,9 @@ pub fn run() -> eyre::Result<()> {
             .launch()
             .await?;
 
-        // TODO
+        // Run to completion
+        node_handle.wait_for_node_exit().await?;
+
         Ok(())
     })
 }
